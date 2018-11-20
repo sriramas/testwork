20d8ace870ef28784b34f19482203a13b5e37c1b
srirama.singeri <srirama.singeri@lge.com>
2018-11-20 19:33:29 +0530

    react app

---
+++ src/AppNew.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class App extends Component {
+	render(){
+		return (
+			<Link>Hello</Link>
+		);
+	}
+}
+
+export default App;
